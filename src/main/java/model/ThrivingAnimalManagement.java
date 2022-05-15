06a8469706662f6636a05f448d6f8d3bbdb9590a
VelmaAkoch <velmaakochi1@gmail.com>
2022-05-16 01:35:31 +0300

    add interface ThrivingAnimalManagement

---
+++ src/main/java/model/ThrivingAnimalManagement.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.List;
+
+public interface ThrivingAnimalManagement {
+
+    void add(ThrivingAnimal thrivingAnimal);
+    void update(ThrivingAnimal thrivingAnimal);
+    ThrivingAnimal getThrivingAnimalById(int id);
+    List<ThrivingAnimal> getThrivingAnimals();
+    void deleteThrivingAnimal(int id);
+    void deleteAllThrivingAnimals();
+}
